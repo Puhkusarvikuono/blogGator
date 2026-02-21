fa52c8bd4431ff3e01f43a2e98d008c46d451a7e
Puhkusarvikuono <tossu18@proton.me>
2026-02-21 15:42:21 +0200

    Tentative struction for commands and basic functionality

--- main.go
+++ main.go
@@ -3,28 +3,105 @@ package main
 import (
 	"fmt"
 	"github.com/Puhkusarvikuono/blogGator/internal/config"
-	"log"
+	"errors"
+	"os"
 )
 
+type state struct {
+	ConfigPtr			*config.Config
+}
+
+type command struct {
+	Name		string
+	Args		[]string
+}
+
+type commands struct {
+	commands	map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+}
+
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return errors.New("The handler expects a single argument, the username.")
+	}
+
+	login := cmd.Args[0]
+
+	s.ConfigPtr.UserName = login
+	//possibility of error?
+	fmt.Println("The user has been set:", login)
+	return nil
+}
+
 func main() {
-	cfg, err := config.ReadConfigJson()
-	if err != nil {
-		log.Fatalf("Error: %v", err)
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Error: Not enough arguments.")
+		os.Exit(1)
 	}
 
-	fmt.Println(cfg)
-
-	err = cfg.SetUser("puhku")
-	if err != nil {
-		log.Fatalf("Error: %v", err)
+	newCommand := command{
+		Name: args[1],
+		Args: []string{},
 	}
 
-
-	cfg, err = config.ReadConfigJson()
-	if err != nil {
-		log.Fatalf("Error: %v", err)
+	if len(args) > 2 {
+		newCommand.Args = args[2:]
 	}
 
-	fmt.Println(cfg)
+	newConfig := config.Config {
+		URL: "https://this.com",
+		UserName: "newUser",
+	}
 
+	newState := &state{
+		ConfigPtr:	&newConfig,
+	}
+
+	
+	newCommands :=	map[string]func(*state, command) error {
+		"login": handlerLogin,
+	}
+
+	if commandFunc, exists := newCommands[newCommand.Name]; exists {
+		err := commandFunc(newState, newCommand)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+	} else {
+			fmt.Println("Invalid command")
+			os.Exit(1)
+	}
+	
+
+
+//	cfg, err := config.ReadConfigJson()
+//	if err != nil {
+//		log.Fatalf("Error: %v", err)
+//	}
+//
+//	fmt.Println(cfg)
+//
+//	err = cfg.SetUser("puhku")
+//	if err != nil {
+//		log.Fatalf("Error: %v", err)
+//	}
+//
+//
+//	cfg, err = config.ReadConfigJson()
+//	if err != nil {
+//		log.Fatalf("Error: %v", err)
+//	}
+//
+//	fmt.Println(cfg)
+//
 }
