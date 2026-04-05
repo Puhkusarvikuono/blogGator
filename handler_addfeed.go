60fb20d6d45035fa83c2e4aa2580a0628af3165f
Puhkusarvikuono <puhkusarvikuono@proton.me>
2026-04-05 11:34:44 +0300

    Add RSS feed to user

---
+++ handler_addfeed.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/Puhkusarvikuono/blogGator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.Args) < 2 {
+		return errors.New("the handler expects a two arguments, the name and url of the feed")
+	}
+
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		fmt.Println("User is not in the database")
+		os.Exit(1)
+	}
+
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.Args[0],
+		Url:       cmd.Args[1],
+		UserID:    user.ID,
+	})
+	if err != nil {
+		fmt.Println("Feed link unsuccesful")
+		os.Exit(1)
+	}
+
+	fmt.Println("Feed linked successfully")
+	fmt.Printf("User name: %s\nFeed name:%s\nFeed url:%s", user.Name, feed.Name, feed.Url)
+
+	return nil
+}
